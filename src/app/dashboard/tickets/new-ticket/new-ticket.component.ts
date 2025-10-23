a417b3d8b2a29e1d47a045a1d56d81f5055c7a5d
ulugbek-abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-10-23 09:05:51 +0200

    new requests

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -13,10 +13,10 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  add = output<{title: string; text: string}>();
 
   onSubmit(title: string, ticketText: string) {
-    console.log('Entered Title: '+ title);
-    console.log('Entered Text: '+ ticketText);
+    this.add.emit({title: title, text: ticketText});
     this.form()?.nativeElement.reset();
   }
 }
